40203178da21ce8a678f895851330d929f44943d
zfersya <zfersya@gmail.com>
2024-03-13 06:58:51 +0700

    first commit

---
+++ modul4_1302223090/KodeBuah.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul4_1302223090
+{
+    internal class KodeBuah
+    {
+    }
+}
